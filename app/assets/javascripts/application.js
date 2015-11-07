d70dc1652550e52ca73a5f7f2730307857a8bc3b
Thu Trang Pham <thuutrangpham@gmail.com>
2015-11-07 13:25:40 -0500

    Adding in bootstrap, flatui and starting the UI

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require bootstrap
+//= require flat-ui
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  resources :story
+
   # Example resource route with options:
   #   resources :products do
   #     member do
