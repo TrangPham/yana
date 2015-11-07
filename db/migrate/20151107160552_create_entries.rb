8df34b136b7a15be9e6da75e3b276acc68c370b1
lifejuggler <jtchoiboy@gmail.com>
2015-11-07 12:15:44 -0500

    schema/columns added

--- db/migrate/20151107160552_create_entries.rb
+++ db/migrate/20151107160552_create_entries.rb
@@ -2,6 +2,10 @@ class CreateEntries < ActiveRecord::Migration
   def change
     create_table :entries do |t|
 
+      t. string :title
+      t.text :content
+      t.references :tag
+      t.references :user
       t.timestamps null: false
     end
   end
